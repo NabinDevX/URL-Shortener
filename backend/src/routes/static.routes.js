7b8186cd231ff73e146c5d04681f5e723a320d81
NabinDevX <iamnabinbera@gmail.com>
2025-10-21 00:58:06 +0530

    Update render path for home page and add initial views for dashboard, logout, profile, and urls

--- backend/src/routes/static.routes.js
+++ backend/src/routes/static.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("pages/home");
 });
 
 router.get("/signup", (req, res) => {
