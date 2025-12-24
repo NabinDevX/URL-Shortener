77bcb2e5cc168df5c93f91e20de94230a4c6c1be
NabinDevX <iamnabinbera@gmail.com>
2025-12-24 23:20:54 +0530

    fix: correct import path for ContentPage component in content script

--- frontend/packages/extension/src/content.jsx
+++ frontend/packages/extension/src/content.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import ContentPage from "@/content/cotent";
+import ContentPage from "./content/content";
 
 const root = document.createElement("div");
 root.id = "url-shortener";
