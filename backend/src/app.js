abf1fa3a58df657e53b0108350279a72a39c9ffe
NabinDevX <iamnabinbera@gmail.com>
2025-12-30 20:17:18 +0530

    refactor: set trust proxy for improved security and handling of client requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use(
   })
 );
 
+app.set("trust proxy", 1);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
